d3fd3b1222197e0b7707ea3bde84be044f5a2ab0
KIMYOUNGGWANG <67254473+KIMYOUNGGWANG@users.noreply.github.com>
2020-10-15 21:07:47 +0900

    JS : navbarmenu

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// navbar 스크롤내리면 색변경
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height
 document.addEventListener('scroll', () => {
@@ -9,6 +10,20 @@ document.addEventListener('scroll', () => {
         navbar.classList.add('navbar--dark');
     } else {
         navbar.classList.remove('navbar--dark');
-
     }
+});
+
+// navbar메뉴 클릭하면 section변경
+
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link==null) {
+        return;
+    }
+    console.log(event.target.dataset.link);
+    const scroll = document.querySelector(link);
+    scroll.scrollIntoView({behavior : "smooth", block: "end"});
 });
